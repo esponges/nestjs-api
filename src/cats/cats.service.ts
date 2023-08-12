df4fa2f8f64923cb7a87d6ec5a49da2f1903b20d
esponges <esponges@gmail.com>
2023-08-12 14:42:50 -0600

    return cats and fix route

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -2,7 +2,18 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CatsService {
-  private cats = [];
+  private cats = [
+    {
+      name: 'Cat1',
+      age: 1,
+      breed: 'Breed1',
+    },
+    {
+      name: 'Cat2',
+      age: 2,
+      breed: 'Breed2',
+    },
+  ];
 
   getAllCats() {
     return this.cats;
