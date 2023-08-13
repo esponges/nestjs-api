cf571e231c424584a4c69449e3c1dc185027b0ec
esponges <esponges@gmail.com>
2023-08-13 15:07:28 -0600

    make jwt module available in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,23 @@ import { CatsService } from './cats/cats.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { UsersService } from './users/users.service';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from './users/users.module';
 
+const jwtConstants = {
+  secret:
+    'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
+};
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AppController, CatsController, AuthController],
-  providers: [AppService, CatsService, AuthService, UsersService, JwtService],
+  providers: [AppService, CatsService, AuthService, UsersService],
 })
 export class AppModule {}
